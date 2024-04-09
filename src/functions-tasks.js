619963a8831be01ce2c4d989760d3f59dcaf5223
vladimirovicp <vladimirovicp70@gmail.com>
2024-04-09 14:37:09 +0400

    pending 8

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,13 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arrayCount = [];
+  funcs.forEach((func) => {
+    arrayCount.push(func.length);
+  });
+
+  return arrayCount;
 }
 
 /**
