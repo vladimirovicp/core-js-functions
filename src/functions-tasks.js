1b8cc143753f7d6eafa594960854cd65521643ee
vladimirovicp <vladimirovicp70@gmail.com>
2024-04-15 19:18:52 +0400

    feat: 1 pending

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -233,8 +233,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return function meFun() {
+    const res = count;
+    count += 1;
+    return res;
+  };
 }
 
 module.exports = {
