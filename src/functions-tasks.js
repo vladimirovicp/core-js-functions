2118d253b8ab1a72ea0328e962ccdf36e46e2deb
vladimirovicp <vladimirovicp70@gmail.com>
2024-04-15 19:20:45 +0400

    final

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -212,8 +212,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...params) => {
+    return fn(...args1, ...params);
+  };
 }
 
 /**
